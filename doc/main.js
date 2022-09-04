e15a1888bb1d7744e404c23ecd33dcebce68f8f4
CF <67676342+caocaofff@users.noreply.github.com>
2022-09-04 12:15:55 +0800

    Add files via upload
    This is the initial version and supports PECMD 4.0

---
+++ doc/main.js
@@ -0,0 +1,21 @@
+﻿
+$(function() {
+    $('.rollbar-totop').on('click', function(e) {
+        $("html,body").animate({ scrollTop: 0 }, "fast");
+    });
+
+});
+
+//滚动条位置控制导航栏
+$(window).scroll(function() { //滚动事件获取//判断滚动条高度
+    var $scroll = $(document).scrollTop();
+
+    if ($scroll > 1) { //判断滚动条高度
+        //  $(".header").css("position","fixed");
+        $(".rollbar-totop").css("display", "list-item");
+    } else {
+        // $(".header").css("position","relative");
+        $(".rollbar-totop").css("display", "none");
+    }
+
+}); 
